13df34a527d14650cee3a8626e57581ef346c5a7
Mel <nope@example.com>
2026-07-30 02:07:53 -0500

    make it dark mode - easier on eyes

--- builder.sh
+++ builder.sh
@@ -1,6 +1,6 @@
 md2html \
 -f -o index.html \
--t "LinkAWho?" \
+-t "LinkAWho?" --style dark \
 --head-append '<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">' \
 --head-append '<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"' \
 --head-append '<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">' \
