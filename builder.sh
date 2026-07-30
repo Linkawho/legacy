44655e8c7b374134e1e5a7caeb9c532a7e580864
Mel <nope@example.com>
2026-07-30 01:57:05 -0500

    fix builder.sh

--- builder.sh
+++ builder.sh
@@ -1,1 +1,7 @@
-md2html -f -o index.html -t "LinkAWho?" --head-append head-override.html LAH.md
+md2html \
+-f -o index.html \
+-t "LinkAWho?" \
+--head-append '<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">' \
+--head-append '<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"' \
+--head-append '<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">' \
+--head-append '<link rel="manifest" href="/site.webmanifest">' LAH.md
