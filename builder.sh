f02638f2cb43c0153e49b0d3ee345baf15c22c4c
Mel <nope@example.com>
2026-07-30 19:54:17 -0500

    HACKY FIX: fix images

--- builder.sh
+++ builder.sh
@@ -4,4 +4,5 @@ md2html \
 --head-append '<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">' \
 --head-append '<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"' \
 --head-append '<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">' \
---head-append '<link rel="manifest" href="/site.webmanifest">' LAW.md
+--head-append '<link rel="manifest" href="/site.webmanifest">' \
+--head-append '<style>.markdown-body img {max-width: 75% !important; background-color: transparent !important;}</style>' LAW.md
